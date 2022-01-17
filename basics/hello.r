07b855274682b3cfc9dc44454e0aec7fc903867f
Sumu54 <sumoalldata@gmail.com>
2022-01-17 11:30:48 -0500

    created basics folder

---
+++ basics/hello.r
@@ -0,0 +1,9 @@
+# date: jan 17, 2022
+# ref: https://www.tutorialspoint.com/r/r_basic_syntax.htm
+# command: Rscript hello.r
+
+myString<-"Hello, World!, this is my first program in R."
+print (myString)
+
+a = 5
+print(a+6)
